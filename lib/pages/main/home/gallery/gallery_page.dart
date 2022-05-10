2d0fec9d12099f1d06c74fd516540f6798c36b40
OtabekIbrohimov <otabekibrokhimov9333@gmail.com>
2022-05-10 11:33:55 +0500

    11 - commit: Otabek => change some services such as I added tree page: intro_page.dart, gallery_page.dart, and sms.verification_page

--- lib/pages/main/home/gallery/gallery_page.dart
+++ lib/pages/main/home/gallery/gallery_page.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class GalleryPage extends StatelessWidget {
   static const String id = "/gallery_page";
@@ -7,7 +9,61 @@ class GalleryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /// #Otabek
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: Get.height / 11.5,
+        title: Text(
+          "Фото",
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: ImageIcon(
+            AssetImage("assets/icons/img_9.png"),
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {},
+              child: Container(
+                height: Get.height / 5.4,
+                width: Get.width / 2.21,
+                child:ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: CachedNetworkImage(
+                    imageUrl:
+                    "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+                    imageBuilder: (context, imageProvider) => Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: imageProvider,
+                            fit: BoxFit.cover,
+                            colorFilter: const ColorFilter.mode(
+                                Colors.red, BlendMode.colorBurn)),
+                      ),
+                    ),
+                    placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) => const Icon(Icons.error),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
--- lib/pages/sms_verification/sms_verification_page.dart
+++ lib/pages/sms_verification/sms_verification_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+import 'sms_verification_controller.dart';
 
 class SmsVerificationPage extends StatelessWidget {
   static const String id = "/sms_verification_page";
@@ -6,8 +10,168 @@ class SmsVerificationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      /// #Otabek
-    );
+    return GetBuilder<SmsVerificationController>(
+        init: SmsVerificationController(),
+
+        builder: (_controller) {
+          return Scaffold(
+            backgroundColor: const Color(0xffEDEDED),
+            body: ListView(
+              children: [
+                SizedBox(
+                  height: Get.height / 19,
+                ),
+                /// First text for warning enter code to textfields
+                Container(
+                  margin: EdgeInsets.only(left: Get.width / 23),
+                  child: const Text(
+                    "Введите код из СМС",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: Get.height / 17.9,
+                ),
+                Center(
+                  /// information for how to register sms code
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Пожалуйста, введите 6-значный код",
+                        style: TextStyle(
+                          fontSize: 13,
+                        ),
+                      ),
+                      const Text(
+                        "подтверждения,который мы вам отправили",
+                        style: TextStyle(
+                          fontSize: 13,
+                        ),
+                      ),
+                      /// picode fields
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          height: Get.height,
+                          width: Get.width,
+                          child: ListView(
+                            children: <Widget>[
+                              const SizedBox(height: 30),
+                              Form(
+                                key: _controller.formKey,
+                                child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0, horizontal: 30),
+                                    child: PinCodeTextField(
+                                      appContext: context,
+                                      pastedTextStyle: TextStyle(
+                                        color: Colors.green.shade600,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      length: 6,
+                                      obscureText: true,
+                                      obscuringCharacter: '*',
+                                      blinkWhenObscuring: true,
+                                      animationType: AnimationType.fade,
+                                      validator: (v) {
+                                        if (v!.length < 3) {
+                                          return "Please full fill";
+                                        } else {
+                                          return null;
+                                        }
+                                      },
+                                      pinTheme: PinTheme(
+                                          shape: PinCodeFieldShape.box,
+                                          borderRadius: BorderRadius.circular(10.12),
+                                          fieldHeight: Get.height / 13.57,
+                                          fieldWidth: Get.width / 8.26,
+                                          activeFillColor: const Color(0xffF7F7F7),
+                                          inactiveColor: const Color(0xffF7F7F7),
+                                          inactiveFillColor: const Color(0xffF7F7F7)),
+                                      cursorColor: Colors.black,
+                                      animationDuration: Duration(milliseconds: 300),
+                                      enableActiveFill: true,
+                                      controller: _controller.textEditingController,
+                                      keyboardType: TextInputType.number,
+                                      boxShadows: const [
+                                        BoxShadow(
+                                          offset: Offset(0, 1),
+                                          color: Colors.black12,
+                                          blurRadius: 10,
+                                        )
+                                      ],
+                                      onCompleted: (v) {
+                                        print("Completed");
+                                      },
+                                      onChanged: (value) {
+                                        print(value);
+                                      },
+                                      beforeTextPaste: (text) {
+                                        print("Allowing to paste $text");
+                                        //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                                        //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                                        return true;
+                                      },
+                                    )),
+                              ),
+                              SizedBox(
+                                height: Get.height / 12,
+                              ),
+                              /// timer calculator
+                              Center(
+                                child:
+                                Text("Отправить код еще раз через ...00:${_controller.limitedTime}",style: TextStyle(fontSize:13,color: Color(0xffC8C8C8) ),),
+                              ),
+                              SizedBox(
+                                height: Get.height / 10,
+                              ),
+                              Container(
+                                height: Get.height / 14.93,
+                                width: Get.width / 1.07,
+                                margin: EdgeInsets.only(
+                                    left: Get.width / 23, right: Get.width / 23),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xff17B700),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                /// Button for check pincode
+                                child: MaterialButton(
+                                  onPressed: () {
+                                    _controller.calculateTime();
+                                    _controller.formKey.currentState!.validate();
+                                    // conditions for validating
+                                    if (_controller.currentText.length != 6 ||
+                                        _controller.currentText != _controller.keyOfVerifaction) {
+                                      Get.snackbar("pincode", "notug'ri parol kiritildi");
+                                    } else {}
+                                  },
+                                  child: Center(
+                                      child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: const [
+                                            Text(
+                                              "Подтверждать",
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ])),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ); });
   }
 }
+
