ae7fc23b62e12afe35bbd4898766a76b1c2ab16c
ZiyovuddinRafikov <muhammadziyovuddin@gmail.com>
2022-05-26 11:12:46 +0500

    commit: region page

--- lib/pages/main/profile/region/region_controller.dart
+++ lib/pages/main/profile/region/region_controller.dart
@@ -1,5 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class RegionController extends GetxController {
-  /// #Doniyor
+  List region = [
+    "Tashkent",
+    "Andijon",
+    "Buxoro",
+    "Guliston",
+    "Jizzah",
+    "Samarqand",
+    "Namangan",
+    "Farg'ona",
+    "Xorazm",
+    "Qashqadaryo",
+    "Surxandaryo",
+    "Navoiy",
+    "Qoraqalpog'iston"
+  ];
+  late Widget dividerChecker;
 }
